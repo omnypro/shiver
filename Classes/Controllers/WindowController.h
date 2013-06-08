154b7f459f083edad1ea5cb98fbe2c5af595223e
Bryan Veloso <bryan@revyver.com>
2013-06-08 12:49:05 -0700

    Trying to bring this in line with how I set up Perpetual.

---
+++ Classes/Controllers/WindowController.h
@@ -0,0 +1,18 @@
+//
+//  WindowController.h
+//  Shiver
+//
+//  Created by Bryan Veloso on 6/8/13.
+//  Copyright (c) 2013 Revyver, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@class OBMenuBarWindow;
+
+@interface WindowController : NSWindowController
+
+@property (assign) IBOutlet NSView *masterView;
+
+
+@end
