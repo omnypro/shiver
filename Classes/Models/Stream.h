e3118026fd2e963394886991f18dd3a8a0147abe
Bryan Veloso <bryan@revyver.com>
2013-06-07 22:00:19 -0700

    Some models for your consideration.

---
+++ Classes/Models/Stream.h
@@ -0,0 +1,13 @@
+//
+//  Stream.h
+//  Shiver
+//
+//  Created by Bryan Veloso on 6/7/13.
+//  Copyright (c) 2013 Revyver, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Stream : NSObject
+
+@end
---
+++ Classes/Models/Stream.m
@@ -0,0 +1,13 @@
+//
+//  Stream.m
+//  Shiver
+//
+//  Created by Bryan Veloso on 6/7/13.
+//  Copyright (c) 2013 Revyver, Inc. All rights reserved.
+//
+
+#import "Stream.h"
+
+@implementation Stream
+
+@end
