c2af92a2d3091e871ca1bca553e13e986b97a407
Bryan Veloso <bryan@revyver.com>
2013-06-08 21:12:40 -0700

    Start on the stream view. Hoping for the best.

---
+++ Classes/Controllers/StreamViewController.m
@@ -0,0 +1,27 @@
+//
+//  StreamViewController.m
+//  Shiver
+//
+//  Created by Bryan Veloso on 6/8/13.
+//  Copyright (c) 2013 Revyver, Inc. All rights reserved.
+//
+
+#import "StreamViewController.h"
+
+@interface StreamViewController ()
+
+@end
+
+@implementation StreamViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
