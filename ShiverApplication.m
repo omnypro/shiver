5200049f16367405a76c3bfdd8400a912d307fea
Bryan Veloso <bryan@revyver.com>
2013-06-09 10:23:09 -0700

    AppDelegate -> ShiverApplication.

--- ShiverApplication.m
+++ ShiverApplication.m
@@ -1,5 +1,5 @@
 //
-//  AppDelegate.m
+//  ShiverApplication.m
 //  Shiver
 //
 //  Created by Bryan Veloso on 6/6/13.
@@ -13,15 +13,3 @@
 @dynamic delegate;
 
 @end
-
-//- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-//{
-//    // Initialize and display the status bar menu item.
-//    // self.statusBar = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
-//    // [self.statusBar setTitle:@"Shiver"];
-//    // [self.statusBar setMenu:self.statusMenu];
-//    // [self.statusBar setHighlightMode:YES];
-//
-//    // @@@ TODO: Set an image.
-//    // [self.statusBar setImage];
-//}
