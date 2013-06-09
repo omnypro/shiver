6a332b30fe1f1ad031c6540854c97a652fe99ee9
Bryan Veloso <bryan@revyver.com>
2013-06-08 21:12:40 -0700

    Start on the stream view. Hoping for the best.

---
+++ Classes/Controllers/StreamViewController.h
@@ -0,0 +1,13 @@
+//
+//  StreamViewController.h
+//  Shiver
+//
+//  Created by Bryan Veloso on 6/8/13.
+//  Copyright (c) 2013 Revyver, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface StreamViewController : NSViewController
+
+@end
