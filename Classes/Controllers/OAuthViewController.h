337ff5ce860ecd8e63d514e997dcbbc9db3b4ee7
Bryan Veloso <bryan@revyver.com>
2013-06-10 00:50:06 -0700

    Create our own URL scheme (shiver://).

---
+++ Classes/Controllers/OAuthViewController.h
@@ -0,0 +1,13 @@
+//
+//  OAuthViewController.h
+//  Shiver
+//
+//  Created by Bryan Veloso on 6/9/13.
+//  Copyright (c) 2013 Revyver, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface OAuthViewController : NSViewController
+
+@end
