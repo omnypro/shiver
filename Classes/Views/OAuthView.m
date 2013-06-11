ff26bc4ff1dfdd572811b1d10eaf31d6815c20a6
Bryan Veloso <bryan@revyver.com>
2013-06-11 11:46:47 -0700

    Lots of work on getting the OAuth window to kinda sorta work. More to go.

---
+++ Classes/Views/OAuthView.m
@@ -0,0 +1,28 @@
+//
+//  OAuthView.m
+//  Shiver
+//
+//  Created by Bryan Veloso on 6/11/13.
+//  Copyright (c) 2013 Revyver, Inc. All rights reserved.
+//
+
+#import "OAuthView.h"
+
+@implementation OAuthView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
