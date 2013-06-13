0d3308c9a3459fb689768cb3d9570848b1fde545
Bryan Veloso <bryan@revyver.com>
2013-06-13 11:35:19 -0700

    Add an EmptyStreamListController.

---
+++ Classes/Controllers/EmptyStreamListController.h
@@ -0,0 +1,13 @@
+//
+//  EmptyStreamListController.h
+//  Shiver
+//
+//  Created by Bryan Veloso on 6/13/13.
+//  Copyright (c) 2013 Revyver, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface EmptyStreamListController : NSViewController
+
+@end
---
+++ Classes/Controllers/EmptyStreamListController.m
@@ -0,0 +1,27 @@
+//
+//  EmptyStreamListController.m
+//  Shiver
+//
+//  Created by Bryan Veloso on 6/13/13.
+//  Copyright (c) 2013 Revyver, Inc. All rights reserved.
+//
+
+#import "EmptyStreamListController.h"
+
+@interface EmptyStreamListController ()
+
+@end
+
+@implementation EmptyStreamListController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
