37db24eb9d4b65b239431c74f31342eab9002dcb
Bryan Veloso <bryan@revyver.com>
2013-06-18 14:28:45 -0700

    Add StreamListView and StreamPreviewImageView.

---
+++ Classes/Views/StreamListView.m
@@ -0,0 +1,28 @@
+//
+//  StreamListView.m
+//  Shiver
+//
+//  Created by Bryan Veloso on 6/18/13.
+//  Copyright (c) 2013 Revyver, Inc. All rights reserved.
+//
+
+#import "StreamListView.h"
+
+@implementation StreamListView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
---
+++ Classes/Views/StreamPreviewImageView.m
@@ -0,0 +1,28 @@
+//
+//  StreamPreviewImageView.m
+//  Shiver
+//
+//  Created by Bryan Veloso on 6/18/13.
+//  Copyright (c) 2013 Revyver, Inc. All rights reserved.
+//
+
+#import "StreamPreviewImageView.h"
+
+@implementation StreamPreviewImageView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
