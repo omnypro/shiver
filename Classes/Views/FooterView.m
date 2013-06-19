aa39d29da1c1546ab3b01cad61d0618a3e8ff3c5
Bryan Veloso <bryan@revyver.com>
2013-06-19 12:43:46 -0700

    We're just drawing boxes. No need for the complexity.

--- Classes/Views/FooterView.m
+++ Classes/Views/FooterView.m
@@ -17,77 +17,25 @@
     // Declare our colors and gradients first. There's a lot of them.
     NSColor *leftTopColor = [NSColor colorWithHex:@"#343434"];;
     NSColor *leftBottomColor = [NSColor colorWithHex:@"#141414"];
-    NSColor *rightTopColor = [NSColor colorWithHex:@"#EEEEEF"];
-    NSColor *rightBottomColor = [NSColor colorWithHex:@"#C8C8CE"];
-    NSColor *leftInnerShadowColor = [NSColor colorWithHex:@"#555555"];
+    NSColor *rightTopColor = [NSColor colorWithHex:@"#E0E0E1"];
+    NSColor *rightBottomColor = [NSColor colorWithHex:@"#C0C0C6"];
+    NSColor *leftHighlightColor = [NSColor colorWithHex:@"#3F3F40"];
     NSGradient *leftGradient = [[NSGradient alloc] initWithStartingColor:leftTopColor endingColor:leftBottomColor];
     NSGradient *rightGradient = [[NSGradient alloc] initWithStartingColor:rightTopColor endingColor:rightBottomColor];
 
-    // Declare our inner shadows.
-    NSShadow* leftHighlight = [[NSShadow alloc] init];
-    [leftHighlight setShadowColor:leftInnerShadowColor];
-    [leftHighlight setShadowOffset:NSMakeSize(0, -1)];
-    [leftHighlight setShadowBlurRadius:0];
-    NSShadow* rightHighlight = [[NSShadow alloc] init];
-    [rightHighlight setShadowColor:[NSColor whiteColor]];
-    [rightHighlight setShadowOffset:NSMakeSize(0, -1)];
-    [rightHighlight setShadowBlurRadius:0];
-
-    // Draw the left side of the two-tone footer first, then apply our
-    // previously declared inner shadow.
+    // Draw the left side of the two-tone footer first.
     NSBezierPath* leftPath = [NSBezierPath bezierPathWithRect:NSMakeRect(0, 0, 38, 36)];
-    [leftGradient drawInBezierPath:leftPath angle:-90];
+    NSBezierPath* leftHighlightPath = [NSBezierPath bezierPathWithRect:NSMakeRect(0, 35, 37, 1)];
+    [leftGradient drawInBezierPath:leftPath angle: -90];
+    [leftHighlightColor setFill];
+    [leftHighlightPath fill];
 
-    NSRect leftRect = NSInsetRect([leftPath bounds], -leftHighlight.shadowBlurRadius, -leftHighlight.shadowBlurRadius);
-    leftRect = NSOffsetRect(leftRect, -leftHighlight.shadowOffset.width, -leftHighlight.shadowOffset.height);
-    leftRect = NSInsetRect(NSUnionRect(leftRect, [leftPath bounds]), -1, -1);
-
-    NSBezierPath* leftNegativePath = [NSBezierPath bezierPathWithRect:leftRect];
-    [leftNegativePath appendBezierPath:leftPath];
-    [leftNegativePath setWindingRule:NSEvenOddWindingRule];
-
-    [NSGraphicsContext saveGraphicsState];
-    {
-        NSShadow* leftHighlightWithOffset = [leftHighlight copy];
-        CGFloat xOffset = leftHighlightWithOffset.shadowOffset.width + round(leftRect.size.width);
-        CGFloat yOffset = leftHighlightWithOffset.shadowOffset.height;
-        leftHighlightWithOffset.shadowOffset = NSMakeSize(xOffset + copysign(0.1, xOffset), yOffset + copysign(0.1, yOffset));
-        [leftHighlightWithOffset set];
-        [[NSColor grayColor] setFill];
-        [leftPath addClip];
-        NSAffineTransform* transform = [NSAffineTransform transform];
-        [transform translateXBy:-round(leftRect.size.width) yBy: 0];
-        [[transform transformBezierPath:leftNegativePath] fill];
-    }
-    [NSGraphicsContext restoreGraphicsState];
-
-    // Draw the right side of the two-tone footer, then apply our previously
-    // declared inner shadow.
-    NSBezierPath* rightPath = [NSBezierPath bezierPathWithRect: NSMakeRect(38, 0, 282, 36)];
+    // Draw the right side of the two-tone footer.
+    NSBezierPath* rightPath = [NSBezierPath bezierPathWithRect:NSMakeRect(38, 0, 282, 36)];
+    NSBezierPath* rightHighlightPath = [NSBezierPath bezierPathWithRect:NSMakeRect(38, 35, 282, 1)];
     [rightGradient drawInBezierPath: rightPath angle: -90];
-
-    NSRect rightRect = NSInsetRect([rightPath bounds], -rightHighlight.shadowBlurRadius, -rightHighlight.shadowBlurRadius);
-    rightRect = NSOffsetRect(rightRect, -rightHighlight.shadowOffset.width, -rightHighlight.shadowOffset.height);
-    rightRect = NSInsetRect(NSUnionRect(rightRect, [rightPath bounds]), -1, -1);
-
-    NSBezierPath* rightNegativePath = [NSBezierPath bezierPathWithRect:rightRect];
-    [rightNegativePath appendBezierPath:rightPath];
-    [rightNegativePath setWindingRule:NSEvenOddWindingRule];
-
-    [NSGraphicsContext saveGraphicsState];
-    {
-        NSShadow* rightHighlightWithOffset = [rightHighlight copy];
-        CGFloat xOffset = rightHighlightWithOffset.shadowOffset.width + round(rightRect.size.width);
-        CGFloat yOffset = rightHighlightWithOffset.shadowOffset.height;
-        rightHighlightWithOffset.shadowOffset = NSMakeSize(xOffset + copysign(0.1, xOffset), yOffset + copysign(0.1, yOffset));
-        [rightHighlightWithOffset set];
-        [[NSColor grayColor] setFill];
-        [rightPath addClip];
-        NSAffineTransform* transform = [NSAffineTransform transform];
-        [transform translateXBy:-round(rightRect.size.width) yBy: 0];
-        [[transform transformBezierPath:rightNegativePath] fill];
-    }
-    [NSGraphicsContext restoreGraphicsState];
+    [[NSColor whiteColor] setFill];
+    [rightHighlightPath fill];
 
     // Draw a "faked" shadow to separate the two sides of the footer.
     NSBezierPath* rectanglePath = [NSBezierPath bezierPathWithRect:NSMakeRect(37, 0, 1, 36)];
