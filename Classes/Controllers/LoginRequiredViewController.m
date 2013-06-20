1174f32e76fceec81fcb541e3ada0c024a21a5ba
Bryan Veloso <bryan@revyver.com>
2013-06-19 19:04:44 -0700

    Add LoginRequiredView[Controller].

---
+++ Classes/Controllers/LoginRequiredViewController.m
@@ -0,0 +1,27 @@
+//
+//  LoginRequiredViewController.m
+//  Shiver
+//
+//  Created by Bryan Veloso on 6/13/13.
+//  Copyright (c) 2013 Revyver, Inc. All rights reserved.
+//
+
+#import "LoginRequiredViewController.h"
+
+@interface LoginRequiredViewController ()
+
+@end
+
+@implementation LoginRequiredViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
