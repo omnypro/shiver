531ebe255fd28df07ecc34cc55b3c408399e2638
Bryan Veloso <bryan@revyver.com>
2013-06-20 17:49:57 -0700

    Add a helper application to launch our Shiver on startup.

---
+++ Helper/ApplicationDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  ShiverHelper
+//
+//  Created by Bryan Veloso on 6/20/13.
+//  Copyright (c) 2013 Revyver, Inc. All rights reserved.
+//
+
+#import "ApplicationDelegate.h"
+
+@implementation ApplicationDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
