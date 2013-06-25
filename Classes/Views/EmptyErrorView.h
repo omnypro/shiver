0e8b233bc5e2c5d730b605aaf3fa87eb44f328bf
Bryan Veloso <bryan@github.com>
2013-06-25 01:47:14 -0700

    Add EmptyErrorView.
    This replaces (once and for all) the more complex attempts in
    StreamListEmptyErrorView and EmptyStreamListViewController.

---
+++ Classes/Views/EmptyErrorView.h
@@ -0,0 +1,17 @@
+//
+//  StreamListEmptyErrorView.h
+//  Shiver
+//
+//  Created by Bryan Veloso on 6/24/13.
+//  Copyright (c) 2013 Revyver, Inc. All rights reserved.
+//
+
+#import "InnerShadowView.h"
+
+@interface EmptyErrorView : InnerShadowView
+
++ (id)init;
+- (NSView *)emptyViewWithTitle:(NSString *)title subTitle:(NSString *)subTitle;
+- (NSView *)errorViewWithTitle:(NSString *)title subTitle:(NSString *)subTitle;
+
+@end
