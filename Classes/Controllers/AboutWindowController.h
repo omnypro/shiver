3031a1ab85e0e7389a30b6d126df7a5935be6bdd
Bryan Veloso <bryan@github.com>
2013-06-26 19:58:57 -0700

    Add AboutWindowController.

---
+++ Classes/Controllers/AboutWindowController.h
@@ -0,0 +1,13 @@
+//
+//  AboutWindowController.h
+//  Shiver
+//
+//  Created by Bryan Veloso on 6/26/13.
+//  Copyright (c) 2013 Revyver, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AboutWindowController : NSWindowController
+
+@end
---
+++ Classes/Controllers/AboutWindowController.m
@@ -0,0 +1,34 @@
+//
+//  AboutWindowController.m
+//  Shiver
+//
+//  Created by Bryan Veloso on 6/26/13.
+//  Copyright (c) 2013 Revyver, Inc. All rights reserved.
+//
+
+#import "AboutWindowController.h"
+
+@interface AboutWindowController ()
+
+@end
+
+@implementation AboutWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    // Implement this method to handle any initialization after your
+    // window controller's window has been loaded from its nib file.
+}
+
+@end
