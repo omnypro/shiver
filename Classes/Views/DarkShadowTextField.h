290270e9a5097967944f99840cb9278744483c4e
Bryan Veloso <bryan@github.com>
2013-06-28 10:11:25 -0700

    We need two ShadowTextFields now.

---
+++ Classes/Views/DarkShadowTextField.h
@@ -0,0 +1,15 @@
+//
+//  DarkShadowTextField.h
+//  Shiver
+//
+//  Created by Bryan Veloso on 6/18/13.
+//  Copyright (c) 2013 Revyver, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DarkShadowTextField : NSTextField
+
+@property (nonatomic, retain) NSShadow *shadow;
+
+@end
