05042f85f052149ceac755d4402c98ed359dd752
Bryan Veloso <bryan@github.com>
2013-06-28 12:52:11 -0700

    Fix the funky display of the Header and Footer view.

--- Classes/Views/FooterView.m
+++ Classes/Views/FooterView.m
@@ -14,16 +14,14 @@
 
 - (void)drawRect:(NSRect)dirtyRect
 {
-    NSRect frame = dirtyRect;
-
     // Declare our colors first. There's a lot of them.
     NSColor *topColor = [NSColor colorWithHex:@"#343434"];;
-    NSColor *bottomColor = [NSColor colorWithHex:@"#141414"];
+    NSColor *bottomColor = [NSColor colorWithHex:@"#1f1f1f"];
     NSColor *highlightColor = [NSColor colorWithHex:@"#3F3F40"];
 
     // Draw the left side of the two-tone footer first.
-    NSRect leftRect = NSMakeRect(0, 0, NSWidth(frame), NSHeight(frame));
-    NSRect leftHighlightRect = NSMakeRect(0, NSHeight(frame) - 2, NSWidth(frame), 1);
+    NSRect leftRect = NSMakeRect(0, 0, 320, 32);
+    NSRect leftHighlightRect = NSMakeRect(0, 30, 320, 1);
     NSGradient *leftGradient = [[NSGradient alloc] initWithStartingColor:topColor endingColor:bottomColor];
     [leftGradient drawInRect:leftRect angle:-90];
     [highlightColor setFill];
