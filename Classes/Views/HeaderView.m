38995d85a026968af1c24c490883b635d8564904
Bryan Veloso <bryan@github.com>
2013-06-28 01:38:34 -0700

    Redrawing the header and footer.

--- Classes/Views/HeaderView.m
+++ Classes/Views/HeaderView.m
@@ -16,34 +16,27 @@
 - (void)drawRect:(NSRect)dirtyRect
 {
     // Abstracted attributes.
-    CGFloat cornerRadius = 5;
+    NSRect frame = dirtyRect;
 
     // Declare our colors first.
-    NSColor* topColor = [NSColor colorWithHex:@"#464646"];
-    NSColor* bottomColor = [NSColor colorWithHex:@"#262626"];
-    NSColor* highlightColor = [NSColor colorWithHex:@"#555"];
+    NSColor *topColor = [NSColor colorWithHex:@"#F4F4F5"];
+    NSColor *bottomColor = [NSColor colorWithHex:@"#CDCED4"];
+    NSColor *highlightColor = [NSColor colorWithHex:@"#FFF"];
+    NSColor *shadowColor = [NSColor colorWithHex:@"9A9B9F"];
 
     // We're only drawing the left side of the two-tone header.
-    NSRect rect = NSMakeRect(0, 0, 38, 34);
-    NSRect innerRect = NSInsetRect(rect, cornerRadius, cornerRadius);
-    NSBezierPath* path = [NSBezierPath bezierPath];
-    [path moveToPoint:NSMakePoint(NSMinX(rect), NSMinY(rect))];
-    [path lineToPoint:NSMakePoint(NSMaxX(rect), NSMinY(rect))];
-    [path lineToPoint:NSMakePoint(NSMaxX(rect), NSMaxY(rect))];
-    [path appendBezierPathWithArcWithCenter: NSMakePoint(NSMinX(innerRect), NSMaxY(innerRect)) radius:cornerRadius startAngle:90 endAngle:180];
-    [path closePath];
+    NSRect rect = NSMakeRect(0, 0, NSWidth(frame), NSHeight(frame));
+    NSGradient *gradient = [[NSGradient alloc] initWithStartingColor:topColor endingColor:bottomColor];
+    [gradient drawInRect:rect angle:-90];
 
-    NSGradient* gradient = [[NSGradient alloc] initWithStartingColor:topColor endingColor:bottomColor];
-    [gradient drawInBezierPath:path angle:-90];
-
-    // Draw a box for the highlight too.
-    NSRect highlightRect = NSMakeRect(4, 33.5, 33, 0.5);
+    // Draw boxes for the highlight and shadow too.
+    NSRect highlightRect = NSMakeRect(0, NSHeight(frame) - 1, NSWidth(frame), 1);
     [highlightColor setFill];
     NSRectFill(highlightRect);
 
-    // Draw a "faked" shadow to separate the two sides of the footer.
-    [bottomColor setFill];
-    NSRectFill(NSMakeRect(37, 0, 1, 33.5));
+    NSRect shadowRect = NSMakeRect(0, 1, NSWidth(frame), 1);
+    [shadowColor setFill];
+    NSRectFill(shadowRect);
 
     [super drawRect:dirtyRect];
 }
