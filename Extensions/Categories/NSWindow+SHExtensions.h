acbc6d000844ce5f34be4566fba4a868a9f01a32
Bryan Veloso <bryan@github.com>
2013-07-06 11:24:37 -0700

    Mmmm, fades.

---
+++ Extensions/Categories/NSWindow+SHExtensions.h
@@ -0,0 +1,16 @@
+//
+//  NSWindow+SHExtensions.h
+//  Shiver
+//
+//  Created by Bryan Veloso on 7/6/13.
+//  Copyright (c) 2013 Revyver, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NSWindow (SHExtensions)
+
+- (IBAction)fadeIn:(id)sender;
+- (IBAction)fadeOut:(id)sender;
+
+@end
