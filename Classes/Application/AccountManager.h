021bf0ef317320d9369ce1aa46e53006a046d61a
Bryan Veloso <bryan@revyver.com>
2014-02-12 12:57:47 -0800

    Add the concept of an AccountManager. (Will handle reachability, login, etc.)

---
+++ Classes/Application/AccountManager.h
@@ -0,0 +1,13 @@
+//
+//  AccountManager.h
+//  Shiver
+//
+//  Created by Bryan Veloso on 2/11/14.
+//  Copyright (c) 2014 Revyver, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AccountManager : NSObject
+
+@end
---
+++ Classes/Application/AccountManager.m
@@ -0,0 +1,13 @@
+//
+//  AccountManager.m
+//  Shiver
+//
+//  Created by Bryan Veloso on 2/11/14.
+//  Copyright (c) 2014 Revyver, Inc. All rights reserved.
+//
+
+#import "AccountManager.h"
+
+@implementation AccountManager
+
+@end
