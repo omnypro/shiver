c4704a5f283527f92334a6cea7531510a75c20ab
Bryan Veloso <bryan@revyver.com>
2014-02-18 10:55:28 -0800

    Add more labels to the stream viewer.

--- Classes/Controllers/StreamViewerViewController.m
+++ Classes/Controllers/StreamViewerViewController.m
@@ -12,9 +12,12 @@
 
 #import "StreamViewerViewController.h"
 
-@interface StreamViewerViewController () {
-    IBOutlet NSTextField *_streamNameLabel;
-}
+@interface StreamViewerViewController ()
+
+@property (nonatomic, strong) StreamViewModel *stream;
+@property (weak) IBOutlet NSTextField *nameLabel;
+@property (weak) IBOutlet NSTextField *statusLabel;
+@property (weak) IBOutlet NSTextField *viewersLabel;
 
 @end
 
@@ -25,13 +28,46 @@
     [super awakeFromNib];
 
     [_webView setFrameLoadDelegate:self];
+
+    RAC(self, nameLabel.stringValue, @"") = [RACSignal
+        combineLatest:@[RACObserve(self, stream.displayName), RACObserve(self, stream.game)]
+        reduce:^id(NSString *displayName, NSString *game) {
+            if (displayName && game) { return [NSString stringWithFormat:@"%@ playing %@", displayName, game]; }
+            else { return @""; }
+        }];
+    RAC(self, statusLabel.attributedStringValue, @"") = [RACObserve(self, stream.status)
+        map:^id(NSString *value) {
+            if (value) { return [self attributedStatusWithString:value]; }
+            else { return @""; }
+        }];
+    RAC(self, viewersLabel.stringValue, @"") = RACObserve(self, stream.viewers);
 }
 
 - (void)setSelectedStream:(StreamViewModel *)stream {
+    self.stream = stream;
+
     NSURLRequest *request = [NSURLRequest requestWithURL:stream.hlsURL];
     [[_webView mainFrame] loadRequest:request];
+}
 
-    [_streamNameLabel setStringValue:stream.name];
+#pragma mark - Appearance
+
+- (NSAttributedString *)attributedStatusWithString:(NSString *)string
+{
+    NSString *truncatedString = [[string componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]] componentsJoinedByString:@" "];
+    NSMutableAttributedString *attrStatus = [[NSMutableAttributedString alloc] initWithString:truncatedString];
+
+    // Tame the line height first.
+    NSMutableParagraphStyle *style = [[NSMutableParagraphStyle alloc] init];
+    [style setLineBreakMode:NSLineBreakByWordWrapping];
+    [style setMaximumLineHeight:16];
+
+    // Send it off.
+    NSMutableDictionary *attributes = [@{
+        NSParagraphStyleAttributeName: style
+    } mutableCopy];
+    [attrStatus addAttributes:attributes range:NSMakeRange(0, [attrStatus length])];
+    return attrStatus;
 }
 
 #pragma mark - WebFrameLoadDelegate Methods
