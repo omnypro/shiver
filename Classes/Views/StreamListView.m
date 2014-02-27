d9cdeaf7ba7b269a7fb1868d6d0a46640b821fa9
Bryan Veloso <bryan@revyver.com>
2014-02-26 19:13:34 -0800

    Redesign the footer of the stream list view. Simplified.

--- Classes/Views/StreamListView.m
+++ Classes/Views/StreamListView.m
@@ -30,24 +30,13 @@
     [[NSColor colorWithHexString:@"#121212" alpha:1.0] setFill];
     NSRectFill(initialRect);
 
-    // Draw the view's footer rectangle and fill it with a gradient.
-    NSColor *footerTopColor = [NSColor colorWithHexString:@"#242428" alpha:1.0];
-    NSColor *footerBottomColor = [NSColor colorWithHexString:@"#1C1D20" alpha:1.0];
-    NSGradient *gradient = [[NSGradient alloc] initWithStartingColor:footerTopColor endingColor:footerBottomColor];
-    NSBezierPath *footerPath = [NSBezierPath bezierPathWithRect:NSMakeRect(0, 0, self.bounds.size.width, 43)];
-    [gradient drawInBezierPath:footerPath angle:-90];
-
     // Draw the view's footer's top border rectangle.
-    NSRect borderRect = NSMakeRect(0, 43, self.bounds.size.width, 3);
-    [[NSColor colorWithHexString:@"#452D89" alpha:1.0] setFill];
-    NSRectFill(borderRect);
-
-    NSRect borderHighlightRect = NSMakeRect(0, 45, self.bounds.size.width, 1);
-    [[NSColor colorWithHexString:@"#5C3DAF" alpha:1.0] setFill];
+    NSRect borderHighlightRect = NSMakeRect(0, 42, self.bounds.size.width, 1);
+    [[NSColor colorWithHexString:@"#141414" alpha:1.0] setFill];
     NSRectFill(borderHighlightRect);
 
-    NSRect borderShadowRect = NSMakeRect(0, 42, self.bounds.size.width, 1);
-    [[NSColor colorWithHexString:@"#151518" alpha:1.0] setFill];
+    NSRect borderShadowRect = NSMakeRect(0, 43, self.bounds.size.width, 1);
+    [[NSColor colorWithHexString:@"#0C0C0C" alpha:1.0] setFill];
     NSRectFill(borderShadowRect);
 
     // Draw the view's right shadow.
