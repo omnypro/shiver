b9279393b232ae58e8d49bb6895e77cdf5f82187
Bryan Veloso <bryan@revyver.com>
2014-03-05 18:08:01 -0800

    Fix an error in code order. Set self.credential AFTER we use -setAuthorizationHeaderWithCredential:.

--- Classes/Networking/TwitchAPIClient.m
+++ Classes/Networking/TwitchAPIClient.m
@@ -36,6 +36,7 @@ NSString * const kClientSecret = @"rji9hs6u0wbj35snosv1n71ou0xpuqi";
     static dispatch_once_t oncePredicate;
     dispatch_once(&oncePredicate, ^{
         _sharedClient = [[self alloc] initWithBaseURL:[NSURL URLWithString:kTwitchBaseURL] clientID:kClientID secret:kClientSecret];
+        _sharedClient.authorizationURL = [NSURL URLWithString:[NSString stringWithFormat:@"%@oauth2/authorize/?client_id=%@&redirect_uri=%@&response_type=token&scope=user_read+user_follows_edit", kTwitchBaseURL, kClientID, kRedirectURI]];
         _sharedClient.credential = [AFOAuthCredential retrieveCredentialWithIdentifier:_sharedClient.serviceProviderIdentifier];
         if (_sharedClient.credential != nil) {
             [_sharedClient setAuthorizationHeaderWithCredential:_sharedClient.credential];
@@ -53,6 +54,7 @@ NSString * const kClientSecret = @"rji9hs6u0wbj35snosv1n71ou0xpuqi";
     [self registerHTTPOperationClass:[AFJSONRequestOperation class]];
     [self setDefaultHeader:@"Accept" value:@"application/json"];
     [self setDefaultHeader:@"Client-ID" value:kClientID];
+
     return self;
 }
 
@@ -60,26 +62,20 @@ NSString * const kClientSecret = @"rji9hs6u0wbj35snosv1n71ou0xpuqi";
 {
     NSMutableDictionary *queryStrings = [@{} mutableCopy];
     for (NSString *qs in [[url fragment] componentsSeparatedByString:@"&"]) {
-        [queryStrings setValue:[[[qs componentsSeparatedByString:@"="][1] stringByReplacingOccurrencesOfString:@"+" withString:@" "] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding] forKey:[qs componentsSeparatedByString:@"="][0]];
+        [queryStrings
+            setValue:[[[qs componentsSeparatedByString:@"="][1]
+            stringByReplacingOccurrencesOfString:@"+" withString:@" "]
+            stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding]
+            forKey:[qs componentsSeparatedByString:@"="][0]];
     }
 
     return queryStrings;
 }
 
-// This overrides AFOAuth2's method, since it's a douchebag.
 - (void)setAuthorizationHeaderWithToken:(NSString *)token ofType:(NSString *)type
 {
+    // This overrides AFOAuth2's method, since it's a douchebag.
     [self setDefaultHeader:@"Authorization" value:[NSString stringWithFormat:@"%@ %@", type, token]];
-}
-
-- (void)logout
-{
-    self.credential = nil;
-    [AFOAuthCredential deleteCredentialWithIdentifier:self.serviceProviderIdentifier];
-
-    // Remove `accessToken` from userDefaults.
-    [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"accessToken"];
-    [[NSUserDefaults standardUserDefaults] synchronize];
 }
 
 - (RACSignal *)authorizeUsingResponseURL:(NSURL *)url
@@ -87,18 +83,34 @@ NSString * const kClientSecret = @"rji9hs6u0wbj35snosv1n71ou0xpuqi";
     DDLogInfo(@"Authentication: Authorizing with provided URL.");
     NSString *accessToken = [self parseQueryStringsFromURL:url][@"access_token"];
     DDLogVerbose(@"Authentication: (Access Token) %@", accessToken);
-    self.credential = [AFOAuthCredential credentialWithOAuthToken:accessToken tokenType:@"OAuth"];
-    [AFOAuthCredential storeCredential:self.credential withIdentifier:self.serviceProviderIdentifier];
-    [self setAuthorizationHeaderWithCredential:self.credential];
 
-    // Store `accessToken` in userDefaults.
-    [[NSUserDefaults standardUserDefaults] setObject:self.credential.accessToken forKey:@"accessToken"];
+    return [[RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        AFOAuthCredential *credential;
+        credential = [AFOAuthCredential credentialWithOAuthToken:accessToken tokenType:@"OAuth"];
+        [AFOAuthCredential storeCredential:credential withIdentifier:self.serviceProviderIdentifier];
+        [self setAuthorizationHeaderWithCredential:credential];
+
+        [subscriber sendNext:credential];
+        [subscriber sendCompleted];
+        return nil;
+    }] doNext:^(AFOAuthCredential *credential) {
+        self.credential = credential;
+
+        // Store `accessToken` in userDefaults.
+        [[NSUserDefaults standardUserDefaults] setObject:credential.accessToken forKey:@"accessToken"];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+    }];
+}
+
+- (void)logout
+{
+    [AFOAuthCredential deleteCredentialWithIdentifier:self.serviceProviderIdentifier];
+    [self setAuthorizationHeaderWithCredential:nil];
+    self.credential = nil;
+
+    // Remove `accessToken` from userDefaults.
+    [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"accessToken"];
     [[NSUserDefaults standardUserDefaults] synchronize];
-
-    RACReplaySubject *subject = [RACReplaySubject subject];
-    [subject sendNext:self.credential];
-    [subject sendCompleted];
-    return [subject deliverOn:[RACScheduler mainThreadScheduler]];
 }
 
 - (RACSignal *)fetchUser
@@ -108,7 +120,7 @@ NSString * const kClientSecret = @"rji9hs6u0wbj35snosv1n71ou0xpuqi";
         map:^id(id responseObject) {
             NSError *error;
             User *user = [MTLJSONAdapter modelOfClass:User.class fromJSONDictionary:responseObject error:&error];
-            return (user != nil) ? [RACSignal return:user] : [RACSignal error:error];
+            return user;
         }];
 }
 
