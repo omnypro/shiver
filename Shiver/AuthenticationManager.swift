4317a8082cd9eedb9166f9ff6640d94cd6139d3d
Bryan Veloso <bryan@velo.so>
2024-07-07 10:25:19 -0700

    Support `ASWebAuthenticationSession`.

---
+++ Shiver/AuthenticationManager.swift
@@ -0,0 +1,47 @@
+//
+//  AuthenticationManager.swift
+//  Shiver
+//
+//  Created by Bryan Veloso on 7/6/24.
+//
+
+import AuthenticationServices
+import Foundation
+import SwiftUI
+
+class AuthenticationManager: NSObject, ASWebAuthenticationPresentationContextProviding {
+    static let shared = AuthenticationManager()
+    
+    private override init() {}
+    
+    func startAuthentication(completion: @escaping (String?, String?, Error?) -> Void) {
+        let authURL = URL(string: "https://getshiver.app/api/auth/authenticate")!
+        let callbackURLScheme = "shiver"
+        let session = ASWebAuthenticationSession(url: authURL, callbackURLScheme: callbackURLScheme) { callbackURL, error in
+            if let error = error {
+                completion(nil, nil, error)
+            } else if let callbackURL = callbackURL {
+                // Extract access and refresh tokens from the callback URL.
+                if let urlComponents = URLComponents(url: callbackURL, resolvingAgainstBaseURL: false),
+                   let queryItems = urlComponents.queryItems,
+                   let accessToken = queryItems.first(where: { $0.name == "access_token" })?.value,
+                   let refreshToken = queryItems.first(where: { $0.name == "refresh_token" })?.value {
+                    completion(accessToken, refreshToken, nil)
+                } else {
+                    completion(nil, nil, nil)
+                }
+            }
+        }
+        
+        session.presentationContextProvider = self
+        session.start()
+    }
+    
+    func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
+        #if os(macOS)
+        return NSApplication.shared.windows.last(where: \.isKeyWindow)!
+        #else
+        return UIApplication.shared.windows.first{ $0.isKeyWindow }!
+        #endif
+    }
+}
