600eec7e73a5bf8af38b34292b942a28f7948573
Bryan Veloso <bryan@velo.so>
2024-07-08 12:55:27 -0700

    CODABLE. WHEW.

--- Shiver/Models/TwitchStream.swift
+++ Shiver/Models/TwitchStream.swift
@@ -5,7 +5,27 @@
 //  Created by Bryan Veloso on 7/8/24.
 //
 
-final class TwitchStream {
+import SwiftData
+
+@Model
+final class TwitchStream: Codable {
+    enum CodingKeys: String, CodingKey {
+        case id
+        case userId
+        case userName
+        case userLogin
+        case gameId
+        case gameName
+        case type
+        case title
+        case viewerCount
+        case startedAt
+        case language
+        case thumbnailURL = "thumbnailUrl"
+        case tags
+        case isMature
+    }
+    
     var id: String
     var userId: String
     var userLogin: String
@@ -13,6 +33,7 @@ final class TwitchStream {
     var gameId: String
     var gameName: String
     var type: String
+    var title: String
     var viewerCount: Int
     var startedAt: String
     var language: String
@@ -20,7 +41,7 @@ final class TwitchStream {
     var tags: [String]
     var isMature: Bool
     
-    init(id: String, userId: String, userLogin: String, userName: String, gameId: String, gameName: String, type: String, viewerCount: Int, startedAt: String, language: String, thumbnailURL: String, tags: [String], isMature: Bool) {
+    init(id: String, userId: String, userLogin: String, userName: String, gameId: String, gameName: String, type: String, title: String, viewerCount: Int, startedAt: String, language: String, thumbnailURL: String, tags: [String], isMature: Bool) {
         self.id = id
         self.userId = userId
         self.userLogin = userLogin
@@ -28,6 +49,7 @@ final class TwitchStream {
         self.gameId = gameId
         self.gameName = gameName
         self.type = type
+        self.title = title
         self.viewerCount = viewerCount
         self.startedAt = startedAt
         self.language = language
@@ -35,25 +57,41 @@ final class TwitchStream {
         self.tags = tags
         self.isMature = isMature
     }
-
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(String.self, forKey: .id)
+        self.userId = try container.decode(String.self, forKey: .userId)
+        self.userName = try container.decode(String.self, forKey: .userName)
+        self.userLogin = try container.decode(String.self, forKey: .userLogin)
+        self.gameId = try container.decode(String.self, forKey: .gameId)
+        self.gameName = try container.decode(String.self, forKey: .gameName)
+        self.type = try container.decode(String.self, forKey: .type)
+        self.title = try container.decode(String.self, forKey: .title)
+        self.viewerCount = try container.decode(Int.self, forKey: .viewerCount)
+        self.startedAt = try container.decode(String.self, forKey: .startedAt)
+        self.language = try container.decode(String.self, forKey: .language)
+        self.thumbnailURL = try container.decode(String.self, forKey: .thumbnailURL)
+        self.tags = try container.decode([String].self, forKey: .tags)
+        self.isMature = try container.decode(Bool.self, forKey: .isMature)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(userId, forKey: .userId)
+        try container.encode(userName, forKey: .userName)
+        try container.encode(gameId, forKey: .gameId)
+        try container.encode(type, forKey: .type)
+        try container.encode(viewerCount, forKey: .viewerCount)
+        try container.encode(startedAt, forKey: .startedAt)
+        try container.encode(language, forKey: .language)
+        try container.encode(thumbnailURL, forKey: .thumbnailURL)
+        try container.encode(tags, forKey: .tags)
+        try container.encode(isMature, forKey: .isMature)
+    }
 }
 
 struct TwitchStreamResponse: Decodable {
-    let data: [Stream]
-    
-    struct Stream: Decodable {
-        let id: String
-        let user_id: String
-        let user_login: String
-        let user_name: String
-        let game_id: String
-        let game_name: String
-        let type: String
-        let viewer_count: Int
-        let started_at: String
-        let language: String
-        let thumbnail_url: String
-        let tags: [String]
-        let is_mature: Bool
-    }
+    let data: [TwitchStream]
 }
